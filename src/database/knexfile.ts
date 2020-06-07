2bce8bdea680d4daa6aa8c93d2405f4dd5837471
Yuri Burk <yuriburk@gmail.com>
2020-06-07 20:05:10 -0300

    chore: Check if is dev

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,21 +1,23 @@
 import path from 'path';
 
+const rootName = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
+
 const config = {
   client: 'sqlite3',
   connection: {
     filename: path.resolve(
       process.cwd(),
-      'dist',
+      rootName,
       'database',
       'database.sqlite'
     ),
   },
   useNullAsDefault: true,
   migrations: {
-    directory: path.resolve(process.cwd(), 'dist', 'database', 'migrations'),
+    directory: path.resolve(process.cwd(), rootName, 'database', 'migrations'),
   },
   seeds: {
-    directory: path.resolve(process.cwd(), 'dist', 'database', 'seeds'),
+    directory: path.resolve(process.cwd(), rootName, 'database', 'seeds'),
   },
 };
 
